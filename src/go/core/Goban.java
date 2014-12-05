8bdb461c4c687805d4637d72a1ad509dcacfcc42
Thomas Buick <zogwarg@gmail.com>
2014-12-05 11:46:39 +0100

    Added constructor for Goban

--- src/go/core/Goban.java
+++ src/go/core/Goban.java
@@ -6,9 +6,27 @@ package go.core;
 public class Goban {
     private int width;
     private int height;
-    private IntersectionCollection intersections;
+    private Intersection[][] intersections;
 
     public Goban(int width, int height) {
-        //TODO Build a Intersection collection containing width * height Intersections
+        this.width = width;
+        this.height = height;
+
+        intersections = new Intersection[width][height];
+
+        for(int i=0;i<width;i++) {
+            for (int j=0;j<height;j++) {
+                intersections[i][j] = new Intersection();
+            }
+        }
+
+        for(int i=0;i<width;i++) {
+            for(int j=0;j<height;j++){
+                if (i-1>0) intersections[i-1][j].addNeighbour(intersections[i][j]);
+                if (i+1<width) intersections[i+1][j].addNeighbour(intersections[i][j]);
+                if (j-1<0) intersections[i][j-1].addNeighbour(intersections[i][j-1]);
+                if (j>height) intersections[i][j+1].addNeighbour(intersections[i][j+1]);
+            }
+        }
     }
 }
