9e657a8a52e844799e5d2f873485fba7d1766be5
Thomas Buick <zogwarg@gmail.com>
2014-12-15 10:22:35 +0100

    Adding Test Cases, and removing Game class

---
+++ test/go/core/GameRecordTest.java
@@ -0,0 +1,15 @@
+package go.core;
+
+import junit.framework.TestCase;
+
+public class GameRecordTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
