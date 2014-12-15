3f8c365cd742be242da72b5f5875b00dbd6b3f28
loic <loic.padier@gmail.com>
2014-12-15 11:03:55 +0100

    StoneChain refactored, added : -add -removeLiberty -die

--- src/go/core/StoneChain.java
+++ src/go/core/StoneChain.java
@@ -1,12 +1,73 @@
 package go.core;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Represents a stone chain, of horizontally and vertically connected stones of a same color
  * It also keeps track ov the horizontal and vertical vacancies;
  * Created by Thomas on 12/4/2014.
  */
 public class StoneChain {
-    IntersectionCollection positions;
-    IntersectionCollection liberties;
+    Set<Intersection> stones;
+    Set<Intersection> liberties;
     Player owner;
+
+    public StoneChain(Set<Intersection> stones, Set<Intersection> liberties, Player owner) {
+        this.stones = stones;
+        this.liberties = liberties;
+        this.owner = owner;
+    }
+
+    public StoneChain(Intersection intersection, Player owner) {
+        stones = new HashSet<Intersection>();
+        stones.add(intersection);
+        this.owner = owner;
+        liberties = new HashSet<Intersection>();
+        liberties.addAll(intersection.getEmptyNeighbors());
+    }
+
+    /**
+     * Creates a new StoneChain by joining two stoneChains
+     * @param stoneChain the second StoneChain
+     * @param playedStone the stone played this turn
+     * @return a new StoneChain created by fusing both StoneChains
+     */
+    public StoneChain add(StoneChain stoneChain, Intersection playedStone) {
+        // Create new StoneChain
+        StoneChain newStoneChain = new StoneChain(stones, liberties, owner);
+        // Fuse stone sets
+        newStoneChain.stones.addAll(stoneChain.stones);
+        // Fuse liberties
+        newStoneChain.liberties.addAll(stoneChain.liberties);
+        // remove played stone from liberties
+        newStoneChain.liberties.remove(playedStone);
+        return newStoneChain;
+    }
+
+    /**
+     * Removes a played Stone from the liberties of a StoneChain
+     * @param playedStone the stone played this turn
+     * @return a new StoneChain without playedStone as a liberty
+     */
+    public StoneChain removeLiberty(Intersection playedStone) {
+        StoneChain newStoneChain = new StoneChain(stones, liberties, owner);
+        newStoneChain.liberties.remove(playedStone);
+        return newStoneChain;
+    }
+
+    /**
+     * Removes a StoneChain and updates liberties of neighboring chains and capture stone count
+     */
+    public void die() {
+        for (Intersection rollingStone : this.stones) {
+            rollingStone.setStoneChain(null);
+            List<StoneChain> adjacentStoneChains = rollingStone.getAdjacentStoneChains();
+            for (StoneChain stoneChain : adjacentStoneChains) {
+                stoneChain.liberties.add(rollingStone);
+            }
+        }
+        this.owner.addCapturedStones(this.stones.size());
+    }
 }
