be6a1e27098dd941d2638d6fd30bcb6946f0e093
Thomas Buick <zogwarg@gmail.com>
2014-12-17 15:57:58 +0100

    Added new Tests for GameTurn

--- test/go/core/GameTurnTest.java
+++ test/go/core/GameTurnTest.java
@@ -13,7 +13,7 @@ public class GameTurnTest extends TestCase {
 
     private Player one;
     private Player two;
-    private Set<Intersection> captured;
+    private Set<Intersection> emptySet;
 
 
     @Before
@@ -21,41 +21,70 @@ public class GameTurnTest extends TestCase {
         super.setUp();
         one = new Player(1);
         two = new Player(2);
-        captured = new HashSet<Intersection>();
+        emptySet = new HashSet<Intersection>();
+    }
+
+    @Test
+    public void testEmptyStatesEquality() throws Exception {
+        GameTurn nine1 = new GameTurn(9,9);
+        GameTurn nine2 = new GameTurn(9,9);
+        GameTurn nineteen1 = new GameTurn(19,19);
+        GameTurn nineteen2 = new GameTurn(19,19);
+
+        assertEquals(nine1,nine2);
+        assertEquals(nineteen1,nineteen2);
+        assertFalse(nineteen1==nine1);
     }
 
     @Test
     public void testTwoSequenceSameStateShouldBeEqual() throws Exception {
-        GameTurn A = new GameTurn(9,9).toNext(1,1,one,captured)
-                .toNext(1, 2, two, captured)
-                .toNext(3, 3, one, captured)
-                .toNext(8, 7, two, captured)
-                .toNext(7, 7, one, captured)
-                .toNext(5, 2, two, captured);
-        GameTurn B = new GameTurn(9,9).toNext(3,3,one,captured)
-                .toNext(5, 2, two, captured)
-                .toNext(1, 1, one, captured)
-                .toNext(1, 2, two, captured)
-                .toNext(7, 7, one, captured)
-                .toNext(8, 7, two, captured);
+        GameTurn A = new GameTurn(9,9).toNext(1,1,one, emptySet)
+                .toNext(1, 2, two, emptySet)
+                .toNext(3, 3, one, emptySet)
+                .toNext(8, 7, two, emptySet)
+                .toNext(7, 7, one, emptySet)
+                .toNext(5, 2, two, emptySet);
+        GameTurn B = new GameTurn(9,9).toNext(3,3,one, emptySet)
+                .toNext(5, 2, two, emptySet)
+                .toNext(1, 1, one, emptySet)
+                .toNext(1, 2, two, emptySet)
+                .toNext(7, 7, one, emptySet)
+                .toNext(8, 7, two, emptySet);
 
         assertEquals(A, B);
     }
 
     @Test(timeout=3000)
-    public void testToNextIsFast() {
+    public void testToNextIsFast() throws Exception {
         long start = System.currentTimeMillis(); //Timeout option not working, bypass  for now
         GameTurn A = new GameTurn(19,19);
         List<GameTurn> list = new ArrayList<GameTurn>();
         for (int i = 0; i < 1000; i++) {
             for (int j = 0; j < 1000; j++) {
-                A = A.toNext(i%19,j%19,one,captured);
-                A = A.toNext((i+8)%19,(j+8)%19,two,captured);
+                A = A.toNext(i%19,j%19,one, emptySet);
+                A = A.toNext((i+8)%19,(j+8)%19,two, emptySet);
             }
         }
         long end = System.currentTimeMillis();
         long delta = end - start;
         assertTrue("One million creation of game turns should take less than 5 seconds, took: "+delta+" milliseconds", delta < 5000 );
+    }
+
+    @Test
+    public void testCapture() throws Exception {
+        GameTurn expected = new GameTurn(9,9).toNext(5,7,one, emptySet);
+        GameTurn toBeTaken = new GameTurn(9,9).toNext(5,5,one, emptySet)
+                .toNext(5,6,one, emptySet)
+                .toNext(5,7,one, emptySet);
+
+        Set<Intersection> takenStones = new HashSet<Intersection>();
+
+        takenStones.add(new Intersection(null,5,5));
+        takenStones.add(new Intersection(null,5,6));
+
+        toBeTaken = toBeTaken.toNext(-1,-1,one,takenStones);
+
+        assertEquals(expected,toBeTaken);
     }
 
 }
