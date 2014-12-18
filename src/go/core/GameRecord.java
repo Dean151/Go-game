71527c259d0ad26128e809997cc792901902abc8
Thomas Buick <zogwarg@gmail.com>
2014-12-18 09:12:29 +0100

    Implemented Double Stack GameRecord

--- src/go/core/GameRecord.java
+++ src/go/core/GameRecord.java
@@ -1,10 +1,54 @@
 package go.core;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 /**
  * Created by Thomas on 12/4/2014.
  */
 public class GameRecord {
-    // Should be a tree of GameTurns, so that true backwards and forwards modifications are possible
-    // Should be able to be saved to, and loaded from a file
-    // Should be able to load a game for any position in the tree
+    private Stack<GameTurn> tail;
+    private Stack<GameTurn> head;
+
+    public GameRecord() {
+        tail = new Stack<GameTurn>();
+        head = new Stack<GameTurn>();
+    }
+
+    public GameRecord(GameRecord record) {
+        this(record.head, record.tail);
+    }
+
+    private GameRecord(Stack<GameTurn> tail , Stack<GameTurn> head) {
+        this.tail = tail;
+        this.head = head;
+    }
+
+    public void apply(GameTurn turn) {
+        tail.push(turn);
+    }
+
+    public void undo() throws EmptyStackException {
+        head.push(tail.pop());
+    }
+
+    public void redo() throws EmptyStackException {
+        tail.push(head.pop());
+    }
+
+    public Iterable<GameTurn> getPlayedGameTurns() {
+        return tail;
+    }
+
+    public boolean save(String filepath) throws Exception {
+        return false;
+        //TODO Implement Save method
+    }
+
+    public boolean load(String filepath) throws Exception {
+        return false;
+        //TODO Implement load method
+    }
 }
+
+
