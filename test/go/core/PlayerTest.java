230b2fb26f23e70f9cfbc45e79c7712938e9aa52
Thomas Buick <zogwarg@gmail.com>
2014-12-18 17:41:58 +0100

    Add basic Tests to player

--- test/go/core/PlayerTest.java
+++ test/go/core/PlayerTest.java
@@ -1,11 +1,26 @@
 package go.core;
 
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 public class PlayerTest extends TestCase {
 
+    Player player = null;
+
+    @Before
     public void setUp() throws Exception {
         super.setUp();
+        player = new Player(1);
+    }
 
+    @Test
+    public void testCaptureCount() {
+        player.addCapturedStones(5);
+        assertEquals(5,player.getCapturedStones());
+        player.addCapturedStones(5);
+        assertEquals(10,player.getCapturedStones());
+        player.removeCapturedStones(7);
+        assertEquals(3,player.getCapturedStones());
     }
 }
