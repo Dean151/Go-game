2324f674e39574206743dc35cb5bed781bcd61c8
Thomas Buick <zogwarg@gmail.com>
2015-01-08 14:44:42 +0100

    Added Tests for Goban

--- test/go/core/GobanTest.java
+++ test/go/core/GobanTest.java
@@ -2,16 +2,33 @@ package go.core;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collections;
+
 import static org.junit.Assert.*;
 
 public class GobanTest {
 
-    private Goban goban;
+    private Goban goban,goban9;
+    private Player one , two ;
 
     @Before
     public void setUp() throws Exception {
         goban = new Goban(3, 3);
+        goban9 = new Goban(9, 9);
+        one = new Player(1);
+        two = new Player(2);
+        goban9.play(goban9.getIntersection(5,5),one);
+        goban9.play(goban9.getIntersection(5,6),two);
+        goban9.play(goban9.getIntersection(6,6),one);
+        goban9.play(goban9.getIntersection(6,7),two);
+        goban9.play(goban9.getIntersection(4,6),one);
+        goban9.play(goban9.getIntersection(4,7),two);
+        goban9.play(goban9.getIntersection(0,0),one);
+        goban9.play(goban9.getIntersection(5,8),two);
+
     }
+
 
     @Test
     public void testIsInGoban() throws Exception {
@@ -37,5 +54,31 @@ public class GobanTest {
         assertTrue(goban.play(intersection, player));
         intersection = new Intersection(goban, 1, 3);
         assertFalse(goban.play(intersection, player));
+    }
+
+    @Test
+    public void testPlayWithKo() throws Exception {
+        assertTrue(goban9.play(goban9.getIntersection(5,7),one));
+        assertFalse(goban9.play(goban9.getIntersection(5,6),two));
+        assertTrue(goban9.play(goban9.getIntersection(2,2),two));
+        assertTrue(goban9.play(goban9.getIntersection(1,1),one));
+        assertTrue(goban9.play(goban9.getIntersection(5,6),two));
+    }
+
+    @Test
+    public void testTakeGameTurn() throws Exception {
+        GameTurn turn = new GameTurn(9,9);
+        turn = turn.toNext(5, 5, 1, Collections.<Intersection>emptySet())
+            .toNext(5,6,2,Collections.<Intersection>emptySet())
+            .toNext(6,6,1,Collections.<Intersection>emptySet())
+            .toNext(6,7,2,Collections.<Intersection>emptySet())
+            .toNext(4,6,1,Collections.<Intersection>emptySet())
+            .toNext(4,7,2,Collections.<Intersection>emptySet())
+            .toNext(0,0,1,Collections.<Intersection>emptySet())
+            .toNext(5,8,2,Collections.<Intersection>emptySet());
+
+        Goban fromTurn = new Goban(9,9);
+        fromTurn.takeGameTurn(turn,one,two);
+        assertEquals(goban9.toString(),fromTurn.toString());
     }
 }
