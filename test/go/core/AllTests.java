dd37cbe809c437da3037708898a58ca9023681a7
Dean151 <thomas.durand.verjat@gmail.com>
2015-01-15 08:26:34 +0100

    Refactoring for lisibility

--- test/go/core/AllTests.java
+++ test/go/core/AllTests.java
@@ -5,7 +5,14 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GameRecordTest.class , GameTurnTest.class, GobanTest.class, IntersectionTest.class, PlayerTest.class, StoneChainTest.class })
+@SuiteClasses({
+        GameRecordTest.class,
+        GameTurnTest.class,
+        GobanTest.class,
+        IntersectionTest.class,
+        PlayerTest.class,
+        StoneChainTest.class
+})
 public class AllTests {
 
 }
