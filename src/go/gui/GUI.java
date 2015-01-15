7d67d234dd1fdfa5e92159c937f7c1968083aa3a
Dean151 <thomas.durand.verjat@gmail.com>
2015-01-15 11:53:09 +0100

    Drawing Goban in GUI

--- src/go/gui/GUI.java
+++ src/go/gui/GUI.java
@@ -7,14 +7,27 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
 
 /**
  * Created by thomas on 15/01/15.
  */
 public class GUI extends JFrame {
-    private static final int TOKEN_SIZE = 35;
+    private static final int TOKEN_INITIAL_SIZE = 35;
     private static final int MENU_SIZE = 60;
+
+    // Initialing sprites
+    private ImageIcon grid_ul = new ImageIcon(new ImageIcon("sprites/ul.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_u = new ImageIcon(new ImageIcon("sprites/u.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_ur = new ImageIcon(new ImageIcon("sprites/ur.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_l = new ImageIcon(new ImageIcon("sprites/l.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_c = new ImageIcon(new ImageIcon("sprites/c.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_spot = new ImageIcon(new ImageIcon("sprites/spot.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_r = new ImageIcon(new ImageIcon("sprites/r.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_bl = new ImageIcon(new ImageIcon("sprites/bl.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_b = new ImageIcon(new ImageIcon("sprites/b.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_br = new ImageIcon(new ImageIcon("sprites/br.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_p1 = new ImageIcon(new ImageIcon("sprites/p1.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
+    private ImageIcon grid_p2 = new ImageIcon(new ImageIcon("sprites/p2.png").getImage().getScaledInstance(TOKEN_INITIAL_SIZE,TOKEN_INITIAL_SIZE, Image.SCALE_FAST));
 
     private Goban goban;
 
@@ -38,6 +51,10 @@ public class GUI extends JFrame {
     private JMenu jMenuAbout;
     private JMenuItem jAboutInfo;
 
+    // Goban
+    private JPanel jGoban;
+    private JButton[][] jIntersections;
+
     /**
      *
      * @param goban goban that correspond to the actual game
@@ -45,8 +62,8 @@ public class GUI extends JFrame {
     public GUI(Goban goban) {
         this.goban = goban;
 
-        this.height = TOKEN_SIZE * goban.getHeight() + MENU_SIZE;
-        this.width = TOKEN_SIZE * goban.getWidth();
+        this.height = TOKEN_INITIAL_SIZE * goban.getHeight() + MENU_SIZE;
+        this.width = TOKEN_INITIAL_SIZE * goban.getWidth();
 
         // Creating menus
         this.initMenu();
@@ -58,7 +75,7 @@ public class GUI extends JFrame {
         this.initWindow();
 
         // printing the goban on screen
-        this.reloadGoban();
+        this.loadGoban();
 
         pack();
     }
@@ -202,7 +219,77 @@ public class GUI extends JFrame {
         jMenuBar.add(jMenuAbout);
     }
 
-    private void reloadGoban() {
-        // TODO draw goban in window
+    /**
+     * Allow to know if the intersection should be marked on the goban
+     * @param x x coord
+     * @param y y coord
+     * @return true if the intersection is marked
+     */
+    private boolean shouldBeSpot(int x, int y) {
+        // TODO math law for spot detection
+        return false;
+    }
+
+
+    /**
+     * Load Goban
+     */
+    private void loadGoban() {
+        // Central layout is a grid
+        int gobanWidth = goban.getWidth();
+        int gobanHeight = goban.getHeight();
+        jGoban = new JPanel(new GridLayout(gobanWidth,gobanHeight));
+
+        // Creating the board
+        jIntersections = new JButton[gobanWidth][gobanHeight];
+
+        // Creating the buttons
+        for(int x=0;x<gobanHeight;x++) {
+            for(int y=0;y<gobanWidth;y++) {
+                // creating button at [x,y]
+                if (x == gobanHeight - 1) {
+                    if (y == 0) {
+                        jIntersections[x][y] = new JButton(grid_ul);
+                    } else if (y == gobanWidth - 1) {
+                        jIntersections[x][y] = new JButton(grid_ur);
+                    } else {
+                        jIntersections[x][y] = new JButton(grid_u);
+                    }
+                } else if (x == 0) {
+                    if (y == 0) {
+                        jIntersections[x][y] = new JButton(grid_bl);
+                    } else if (y == gobanWidth - 1) {
+                        jIntersections[x][y] = new JButton(grid_br);
+                    } else {
+                        jIntersections[x][y] = new JButton(grid_b);
+                    }
+                } else {
+                    if (y == 0) {
+                        jIntersections[x][y] = new JButton(grid_l);
+                    } else if (y == gobanWidth - 1) {
+                        jIntersections[x][y] = new JButton(grid_r);
+                    } else {
+                        if (shouldBeSpot(x,y)) {
+                            jIntersections[x][y] = new JButton(grid_spot);
+                        } else {
+                            jIntersections[x][y] = new JButton(grid_c);
+                        }
+                    }
+                }
+
+                jIntersections[x][y].setEnabled(true);
+                jIntersections[x][y].setBorder(BorderFactory.createEmptyBorder());
+                jIntersections[x][y].setContentAreaFilled(false);
+
+                jIntersections[x][y].setMaximumSize(new java.awt.Dimension(100, 100));
+                jIntersections[x][y].setMinimumSize(new java.awt.Dimension(10, 10));
+                jIntersections[x][y].setPreferredSize(new java.awt.Dimension(TOKEN_INITIAL_SIZE, TOKEN_INITIAL_SIZE));
+
+                // Adding button
+                jGoban.add(jIntersections[x][y],x,y);
+            }
+        }
+        // Adding goban in Main content panel
+        getContentPane().add(jGoban, BorderLayout.CENTER);
     }
 }
