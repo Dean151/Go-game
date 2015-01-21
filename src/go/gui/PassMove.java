7fe7f5b59d829401f3d752d0f493854655186082
Thomas Buick <zogwarg@gmail.com>
2015-01-21 20:58:32 +0100

    Implemented rudimentary game end

--- src/go/gui/PassMove.java
+++ src/go/gui/PassMove.java
@@ -1,5 +1,9 @@
 package go.gui;
 
+import go.core.Goban;
+import go.core.Player;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -28,7 +32,29 @@ public class PassMove implements ActionListener {
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        gui.getGoban().pass(gui.getGoban().getPlayer());
+        Goban goban = gui.getGoban();
+        goban.pass(goban.getPlayer());
         System.out.println("Pass applied");
+        if (goban.getSuccessivePassCount() > 2 ) {
+            Player odd = goban.getPlayer();
+            int oddCount = odd.getCapturedStones();
+            String oddPlural = (oddCount != 1 ? "s" : "" );
+
+            goban.nextPlayer();
+
+            Player even = goban.getPlayer();
+            int evenCount = even.getCapturedStones();
+            String evenPlural = (evenCount != 1 ? "s" : "" );
+
+            goban.precedentPlayer();
+
+            JOptionPane.showMessageDialog(gui,
+                    oddCount + " pierre" + oddPlural + " de " + odd + " ont été capturée" + oddPlural + "\n" +
+                    evenCount + " pierre" + evenPlural + " de " + even + " ont été capturée" + evenPlural + "\n" ,
+                    "Fin de partie",
+                    JOptionPane.PLAIN_MESSAGE);
+
+        }
+
     }
 }
