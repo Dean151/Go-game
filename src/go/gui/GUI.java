4212f3d8df075e1dfcdd80a9478f04e6df8012f1
Thomas Buick <zogwarg@gmail.com>
2015-01-21 21:53:56 +0100

    Unused imports

--- src/go/gui/GUI.java
+++ src/go/gui/GUI.java
@@ -9,8 +9,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 /**
  * Created by thomas on 15/01/15.
